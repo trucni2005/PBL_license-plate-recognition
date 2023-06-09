6b80810f433e0fcf7c71b5362d2356f09c173bd0
Đặng Thị Trúc Ni <102399765+trucni2005@users.noreply.github.com>
2023-06-10 02:38:09 +0700

    commit code

--- DataSet/temp/py.py
+++
@@ -1,9 +0,0 @@
-import os
-
-counter = 28  # Đặt giá trị ban đầu của counter là 28
-
-for filename in os.listdir("."):  # Lặp qua tất cả các tệp trong thư mục hiện tại
-    if filename.endswith(".txt"):  # Nếu tệp là tệp hình ảnh .jpg
-        new_filename = "{}.txt".format(counter)  # Đặt tên mới cho tệp
-        os.rename(filename, new_filename)  # Đổi tên tệp
-        counter += 1  # Tăng giá trị của counter lên 1
